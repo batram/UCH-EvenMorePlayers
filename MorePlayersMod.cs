f0e071e9d2bb0b8d61abc0e12d38b3a08d40147f
batram <ym@batr.am>
2022-05-04 01:04:17 +0200

    Local 5 player versus
    Work with plenty of errors in the background

--- MorePlayersMod.cs
+++ MorePlayersMod.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 using System.Reflection;
 using System.Reflection.Emit;
 using BepInEx;
@@ -7,14 +8,17 @@ using UnityEngine;
 
 namespace MorePlayers
 {
-    [BepInPlugin("notfood.MorePlayers", "MorePlayers", "1.0.0.0")]
+    [BepInPlugin("notfood.MorePlayers", "EvenMorePlayers", "0.0.0.1")]
     public class MorePlayersMod : BaseUnityPlugin
     {
         void Awake()
         {
+            PlayerManager.maxPlayers = 8;
+
             new Harmony("notfood.UltimateBuilder").PatchAll();
 
             Debug.Log("[MorePlayersMod] started.");
+            Debug.Log("[GameSettings.GetInstance().MaxPlayers] " + GameSettings.GetInstance().MaxPlayers + "; [PlayerManager.maxPlayers] " + PlayerManager.maxPlayers);
         }
     }
 
@@ -33,15 +37,75 @@ namespace MorePlayers
             yield return AccessTools.Method(typeof(KickTracker), nameof(KickTracker.CountVotes));
             yield return AccessTools.Method(typeof(KickTracker), nameof(KickTracker.VotesFromNetworkNumber));
             yield return AccessTools.Method(typeof(KeyboardInput), nameof(KeyboardInput.Reset));
+            yield return AccessTools.Method(typeof(VersusControl), "get_playersLeftToPlace");
+
+            yield return AccessTools.Method(typeof(Controller), nameof(Controller.AddPlayer));
+            yield return AccessTools.Method(typeof(ControllerDisconnect), nameof(ControllerDisconnect.SetPromptForPlayer));
+            yield return AccessTools.Method(typeof(GraphScoreBoard), nameof(GraphScoreBoard.SetPlayerCount));
+
+            yield return AccessTools.Method(typeof(InventoryBook), nameof(InventoryBook.HasCursor));
+            yield return AccessTools.Method(typeof(InventoryBook), nameof(InventoryBook.GetCursor));
+            yield return AccessTools.Method(typeof(InventoryBook), nameof(InventoryBook.AddPlayer));
+
+            yield return AccessTools.Method(typeof(PartyBox), nameof(PartyBox.SetPlayerCount));
         }
-        
+
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> e)
         {
-            foreach(var inst in e)
+            foreach (var inst in e)
             {
                 if (inst.opcode == OpCodes.Ldc_I4_4)
                 {
                     inst.opcode = OpCodes.Ldc_I4_8;
+                }
+                yield return inst;
+            }
+        }
+    }
+
+    [HarmonyPatch]
+    static class SwitchFirst4For8Patch
+    {
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method(typeof(PartyBox), nameof(PartyBox.AddPlayer));
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> e)
+        {
+            var count = 0;
+
+            foreach (var inst in e)
+            {
+                if (inst.opcode == OpCodes.Ldc_I4_4 && count == 0)
+                {
+                    inst.opcode = OpCodes.Ldc_I4_8;
+                    count += 1;
+                }
+                yield return inst;
+            }
+        }
+    }
+
+
+    [HarmonyPatch]
+    static class Switch3For7Patch
+    {
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method(typeof(Controller), nameof(Controller.GetLastPlayerNumber));
+            yield return AccessTools.Method(typeof(Controller), nameof(Controller.GetLastPlayerNumberAfter));
+
+            yield return AccessTools.Method(typeof(GraphScoreBoard), nameof(GraphScoreBoard.SetPlayerCharacter));
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> e)
+        {
+            foreach (var inst in e)
+            {
+                if (inst.opcode == OpCodes.Ldc_I4_3)
+                {
+                    inst.opcode = OpCodes.Ldc_I4_7;
                 }
                 yield return inst;
             }
@@ -93,22 +157,71 @@ namespace MorePlayers
         }
     }
 
-    [HarmonyPatch(typeof(GraphScoreBoard), MethodType.Constructor)]
+    [HarmonyPatch(typeof(GraphScoreBoard), nameof(GraphScoreBoard.SetPlayerCount))]
     static class GraphScoreBoardCtorPatch
     {
-        static void Postfix(GraphScoreBoard __instance)
+        static bool Prefix(GraphScoreBoard __instance, int numberPlayers)
         {
-            __instance.ScorePositions = new RectTransform[8];
+            Array.Resize<RectTransform>(ref __instance.ScorePositions, PlayerManager.maxPlayers);
+
+            __instance.playerScoreLines = new ScoreLine[numberPlayers];
+
+            Debug.LogError("GraphScoreBoard.SetPlayerCount");
+            Vector3 vector = __instance.ScorePositions[0].position + new Vector3(0f, 1.25f, 0f);
+            for (int num = 0; num != numberPlayers; num++)
+            {
+                /* add ScorePositions for additional players */
+                GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(__instance.scoreLinePrefab.gameObject, vector - new Vector3(0f, (float)num * 1.25f, 0f), Quaternion.identity);
+                gameObject.transform.SetParent(__instance.mainParent);
+                gameObject.transform.localScale = Vector3.one;
+                __instance.playerScoreLines[num] = gameObject.GetComponent<ScoreLine>();
+                __instance.playerScoreLines[num].scoreBoardParent = __instance;
+            }
+
+            // skip function
+            return false;
+        }
+
+        static void Postfix(GraphScoreBoard __instance, int numberPlayers)
+        {
         }
     }
 
-    [HarmonyPatch(typeof(LevelSelectController), MethodType.Constructor)]
+    [HarmonyPatch(typeof(LevelSelectController), nameof(LevelSelectController.Awake))]
     static class LevelSelectControllerCtorPatch
     {
         static void Postfix(LevelSelectController __instance)
         {
+            Debug.Log("fixed LevelSelectController");
             __instance.JoinedPlayers = new LobbyPlayer[8];
+
+            if (__instance.PlayerJoinIndicators.Length < PlayerManager.maxPlayers)
+            {
+                int num = __instance.PlayerJoinIndicators.Length;
+                Array.Resize<playerJoinIndicator>(ref __instance.PlayerJoinIndicators, PlayerManager.maxPlayers);
+                for (int i = num; i < __instance.PlayerJoinIndicators.Length; i++)
+                {
+                    /* map PlayerJoinIndicators to 0..3, we reuse existing 4 indicators for multiple players */
+                    __instance.PlayerJoinIndicators[i] = __instance.PlayerJoinIndicators[i % num];
+                }
+            }
+
+            Debug.Log("CursorSpawnPoint.Length " + __instance.CursorSpawnPoint.Length);
+            if (__instance.CursorSpawnPoint.Length < PlayerManager.maxPlayers)
+            {
+                int num = __instance.CursorSpawnPoint.Length;
+                Array.Resize(ref __instance.CursorSpawnPoint, PlayerManager.maxPlayers);
+                for (int i = num; i < __instance.CursorSpawnPoint.Length; i++)
+                {
+                    /* map CursorSpawnPoint to 0..3
+						TODO: add new positions, so cursors aren't hidden behind each other on spawn
+					 */
+                    __instance.CursorSpawnPoint[i] = __instance.CursorSpawnPoint[i % num];
+                }
+            }
+
         }
+
     }
 
     [HarmonyPatch(typeof(LobbyPointCounter), MethodType.Constructor)]
@@ -136,8 +249,92 @@ namespace MorePlayers
     {
         static void Postfix(VersusControl __instance)
         {
+            Debug.Log("patch VersusControl");
             __instance.winOrder = new GamePlayer[8];
             __instance.RemainingPlacements = new int[8];
+        }
+    }
+
+    [HarmonyPatch(typeof(LobbyManager), nameof(LobbyManager.Awake))]
+    static class LobbyManagerCtorPatch
+    {
+        static void Postfix(LobbyManager __instance)
+        {
+            Debug.Log("LobbyManager.instance.lobbySlots " + __instance.lobbySlots.Length);
+
+            __instance.maxPlayers = PlayerManager.maxPlayers;
+            __instance.maxPlayersPerConnection = PlayerManager.maxPlayers;
+
+            if (__instance.lobbySlots.Length < PlayerManager.maxPlayers)
+            {
+                Array.Resize(ref __instance.lobbySlots, PlayerManager.maxPlayers);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(GameSettings), nameof(GameSettings.GetInstance))]
+    static class GameSettingsCtorPatch
+    {
+        static void Postfix(GameSettings __result)
+        {
+            __result.MaxPlayers = PlayerManager.maxPlayers;
+
+            if (__result.PlayerColors.Length < PlayerManager.maxPlayers)
+            {
+                int num2 = __result.PlayerColors.Length;
+                Array.Resize<Color>(ref __result.PlayerColors, PlayerManager.maxPlayers);
+                Color[] playerColors = __result.PlayerColors;
+                for (int j = num2; j < playerColors.Length; j++)
+                {
+                    /* Random generate colors for new players */
+                    __result.PlayerColors[j] = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+                }
+            }
+
+        }
+    }
+
+
+    [HarmonyPatch(typeof(InventoryBook), nameof(InventoryBook.AddPlayer))]
+    static class InventoryBookCtorPatch
+    {
+        static void Prefix(InventoryBook __instance)
+        {
+            Debug.Log("InventoryBook cursorSpawnLocation fix");
+            if (__instance.cursorSpawnLocation.Length < PlayerManager.maxPlayers)
+            {
+                int num2 = __instance.cursorSpawnLocation.Length;
+                Array.Resize<Transform>(ref __instance.cursorSpawnLocation, PlayerManager.maxPlayers);
+                for (int j = num2; j < __instance.cursorSpawnLocation.Length; j++)
+                {
+                    __instance.cursorSpawnLocation[j] = __instance.cursorSpawnLocation[0];
+                }
+
+            }
+
+            //cursorSpawnLocation
+        }
+    }
+
+
+    [HarmonyPatch(typeof(ControllerDisconnect), MethodType.Constructor)]
+    static class ControllerDisconnectCtorPatch
+    {
+        static void Prefix(ControllerDisconnect __instance)
+        {
+            Array.Resize<XboxReconnectPrompt>(ref __instance.ConnectPrompts, PlayerManager.maxPlayers);
+            __instance.orphanedReceivers = new List<InputReceiver>[]
+            {
+                new List<InputReceiver>(),
+                new List<InputReceiver>(),
+                new List<InputReceiver>(),
+                new List<InputReceiver>(),
+                new List<InputReceiver>(),
+                new List<InputReceiver>(),
+                new List<InputReceiver>()
+            };
+            //___showingPrompts = new bool[PlayerManager.maxPlayers];
+            __instance.orphanedCharacters = new Character.Animals[PlayerManager.maxPlayers][];
         }
     }
 }
