16e32d5f7cc649a3888245159e0ef8d225fba594
batram <ym@batr.am>
2022-05-13 21:34:48 +0200

    Get all the log messages and full stack straces
    If fullDebug is set to true, spam the logs with all the things.

---
+++ Patches/FullDebug.cs
@@ -0,0 +1,28 @@
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MorePlayers
+{
+    [HarmonyPatch(typeof(LevelSelectController), nameof(LevelSelectController.SetupLobbyAfterWait))]
+    [HarmonyPatch(typeof(StartGameState), nameof(StartGameState.Awake))]
+    static class StartGameStateCtorPatch
+    {
+        static void Postfix()
+        {
+            if (MorePlayersMod.fullDebug)
+            {
+                LogFilter.currentLogLevel = 0;
+                Debug.logger.logEnabled = true;
+
+                Debug.Log("StartGameStateCtorPatch");
+                Application.SetStackTraceLogType(LogType.Error, StackTraceLogType.Full);
+                Application.SetStackTraceLogType(LogType.Assert, StackTraceLogType.Full);
+                Application.SetStackTraceLogType(LogType.Warning, StackTraceLogType.Full);
+                Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.Full);
+                Application.SetStackTraceLogType(LogType.Exception, StackTraceLogType.Full);
+
+            }
+        }
+    }
+}
