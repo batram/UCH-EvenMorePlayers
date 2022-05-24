af5c3f287cd97665b4d5315e498a21bdbb4fef6f
batram <ym@batr.am>
2022-05-24 11:22:29 +0200

    Menu buttons: finetune image positions

--- Patches/MenuPatch.cs
+++ Patches/MenuPatch.cs
@@ -60,17 +60,19 @@ namespace MorePlayers
                 GameObject play_local = GameObject.Find("main Buttons/Play");
                 var local_label = play_local.transform.Find("Text Label").GetComponent<TabletTextLabel>();
                 local_label.text = "Local";
-                local_label.transform.position -= new Vector3(0.6655f, 0f, 0f);
+                local_label.transform.position -= new Vector3(0.65f, 0f, 0f);
                 var local_image = play_local.transform.Find("Image");
                 local_image.transform.localScale = new Vector3(0.7073f, 0.7073f, 1f);
+                local_image.transform.position -= new Vector3(0.04f, 0f, 0f);
 
                 //Adjust online button
                 GameObject play_online = GameObject.Find("main Buttons/Play Online");
                 var online_label = play_online.transform.Find("Text Label").GetComponent<TabletTextLabel>();
                 online_label.text = "Online";
-                online_label.transform.position -= new Vector3(0.6655f, 0f, 0f);
+                online_label.transform.position -= new Vector3(0.7f, 0f, 0f);
                 var online_image = play_online.transform.Find("Image");
                 online_image.transform.localScale = new Vector3(0.8073f, 0.8073f, 1f);
+                online_image.transform.position -= new Vector3(0.11f, 0f, 0f);
 
                 //Add more button
                 more_button = UnityEngine.Object.Instantiate<GameObject>(play_online);
@@ -84,13 +86,13 @@ namespace MorePlayers
 
                 var more_image = more_button.transform.Find("Image");
                 more_image.transform.localScale = new Vector3(-0.5073f, 0.5073f, 1f);
-                more_image.transform.position += new Vector3(-0.1f, 0.5073f, 0f);
-                var more_image1 = UnityEngine.Object.Instantiate<GameObject>(more_image.gameObject, more_image.transform.parent);
+                more_image.transform.position += new Vector3(-0.06f, 0.5073f, 0f);
+                var more_image1 = Object.Instantiate<GameObject>(more_image.gameObject, more_image.transform.parent);
                 more_image1.transform.position = more_image.transform.position;
                 more_image1.transform.position -= new Vector3(0.6f, 1.2f, 0f);
                 more_image1.transform.localScale = new Vector3(-0.5073f, 0.5073f, 1f);
 
-                var more_image2 = UnityEngine.Object.Instantiate<GameObject>(more_image.gameObject, more_image.transform.parent);
+                var more_image2 = Object.Instantiate<GameObject>(more_image.gameObject, more_image.transform.parent);
                 more_image2.transform.position = more_image.transform.position;
                 more_image2.transform.position -= new Vector3(-0.6f, 1.2f, 0f);
                 more_image2.transform.localScale = new Vector3(-0.5073f, 0.5073f, 1f);
